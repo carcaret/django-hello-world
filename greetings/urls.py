d50b7610d6514148f3ff8547c2dcf2a7a1ed5519
Carlos Carbonell Etayo <carcaret@icloud.com>
2017-07-30 19:22:48 +0200

    Perform AJAX request a read value back

--- greetings/urls.py
+++ greetings/urls.py
@@ -3,9 +3,11 @@ from django.conf.urls import url
 from .views import FormHomePageView 
 from .views import AboutPageView 
 from .views import GreetingPageView 
+from .views import AjaxGreetingPageView 
 
 urlpatterns = [
 	url(r'^$', FormHomePageView.as_view()),
         url(r'^greeting$', GreetingPageView.as_view()),
+        url(r'^ajaxGreeting$', AjaxGreetingPageView.as_view()),
 	url(r'^about/?$', AboutPageView.as_view()),
 ]
--- greetings/views.py
+++ greetings/views.py
@@ -1,11 +1,18 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
 from django.views.generic import FormView
+from django.http import JsonResponse
 from .forms import HelloForm
+import json
 
 class HomePageView(TemplateView):
     def get(self, request, **kwargs):
         return render(request, 'index.html', {'form': HelloForm()})
+
+class FormHomePageView(FormView):
+    template_name = 'index.html'
+    form_class = HelloForm
+    success_url = '/greeting'
 
 class AboutPageView(TemplateView):
     template_name = 'about.html'
@@ -18,8 +25,9 @@ class GreetingPageView(TemplateView):
             name = form.cleaned_data['your_name']
         return render(request, 'greeting.html', {'name': name})
 
-class FormHomePageView(FormView):
-    template_name = 'index.html'
-    form_class = HelloForm
-    success_url = '/greeting'
+class AjaxGreetingPageView(TemplateView):
+    def post(self, request, **kwargs):
+        print(request.body)
+        json_data = json.loads(request.body)
+        return JsonResponse({'name': json_data.get('new_name', 'missing')})
 
