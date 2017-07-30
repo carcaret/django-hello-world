0aee75e252c94c3ae52f8b54e7176f6d91a3f68b
Carlos Carbonell Etayo <carcaret@macpro.local>
2017-07-30 17:32:26 +0200

    Form with FormView

--- greetings/views.py
+++ greetings/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
+from django.views.generic import FormView
 from .forms import HelloForm
 
 class HomePageView(TemplateView):
@@ -16,4 +17,9 @@ class GreetingPageView(TemplateView):
         if form.is_valid():
             name = form.cleaned_data['your_name']
         return render(request, 'greeting.html', {'name': name})
+
+class FormHomePageView(FormView):
+    template_name = 'index.html'
+    form_class = HelloForm
+    success_url = '/greeting'
 
